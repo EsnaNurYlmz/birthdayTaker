9d3aae278c58fe8ed9745aec8e04ec32564fdb27
EsnaNurYlmz <esnanuryilmaz407@gmail.com>
2024-02-08 14:19:09 +0300

    second commit

--- birthdayTaker/ViewController.swift
+++ birthdayTaker/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Esna nur Yılmaz on 30.05.2023.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -20,8 +21,46 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       print(hello world)
+        
+        let storyName = UserDefaults.standard.object(forKey: "name")
+        let storyBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        if let newName = storyName as? String{
+            nameLabel.text = newName
+        }
+        
+        if let newBirthday = storyBirthday as? String{
+            birthdayLabel.text = newBirthday
+            
+        }
     }
-
-
+    
+    @IBAction func saveClicked(_ sender: Any) {
+        
+        UserDefaults.standard.set(nameTextField.text!, forKey: "name") //userdefault ile veriyi hafızaya kaydedilir.
+        UserDefaults.standard.set(birthdayTextField.text!, forKey: "birthday")
+        
+        nameLabel.text = "Name : \(nameTextField.text!)" //namelabel içine nametextfield daki veriyi yazdırması sağlandı.
+        birthdayLabel.text = "Birthday : \(birthdayTextField.text!)"
+    }
+    
+    
+    @IBAction func DeleteClicked(_ sender: Any) {
+        
+        let storyName = UserDefaults.standard.object(forKey: "name") //hafızaya kaydettiğin veriyi silen  kod.
+        let storyBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        if (storyName as? String) != nil {
+            UserDefaults.standard.removeObject(forKey: "name")
+            nameLabel.text = "name: "
+        }
+        
+        if (storyBirthday as? String) != nil {
+            UserDefaults.standard.removeObject(forKey: "birthday")
+            birthdayLabel.text = "birthday: "
+            
+        }
+        
+    }
+    
 }
